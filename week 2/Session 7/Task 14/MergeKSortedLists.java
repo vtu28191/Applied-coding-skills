bcd42d652995e4afb62727664e733f5b543ba78c
vtu28191 <vtu28191@veltech.edu.in>
2026-08-27 09:37:56 +0530

    Create MergeKSortedLists

---
+++ week 2/Session 7/Task 14/MergeKSortedLists.java
@@ -0,0 +1,32 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Add first node of every list
+        for (ListNode list : lists) {
+            if (list != null) {
+                pq.offer(list);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+
+            current.next = node;
+            current = current.next;
+
+            // Add next node from the same list
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
