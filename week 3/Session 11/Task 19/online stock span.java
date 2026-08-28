e195594a5600ca0c7fd348db482c2b8749a548b5
vtu28191 <vtu28191@veltech.edu.in>
2026-08-28 22:11:40 +0530

    Add files via upload

---
+++ week 3/Session 11/Task 19/online stock span.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class StockSpanner {
+
+    Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+
+        stack.push(new int[]{price, span});
+
+        return span;
+    }
+
+    public static void main(String[] args) {
+
+        StockSpanner spanner = new StockSpanner();
+
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+
+        for (int price : prices) {
+            System.out.print(spanner.next(price) + " ");
+        }
+    }
+}
+
+
+
+Output
+1 1 1 2 1 4 6
