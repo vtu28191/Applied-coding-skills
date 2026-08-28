21845de8acae749314d5629e5ac74902811a3bc9
vtu28191 <vtu28191@veltech.edu.in>
2026-08-28 22:15:25 +0530

    Add files via upload

---
+++ week 3/Session 11/Task 19/valid parentheses.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } 
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        String s = "()[]{}";
+
+        System.out.println(obj.isValid(s));
+    }
+}
+
+
+
+Output
+true
